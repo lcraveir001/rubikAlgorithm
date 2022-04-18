32d7a1300b65f31caa500208873c6fc99274e925
lilac <lilacrave01@gmail.com>
2022-04-18 11:51:30 -0500

    Co-authored-by: Cora  <afortiori@users.noreply.github.com> Co-authored-by: jiekeller <jiekeller@users.noreply.github.com> Co-authored-by: Isaac Wan <Izooack@users.noreply.github.com>

---
+++ Algorithm.java
@@ -0,0 +1,26 @@
+//hello every one 
+//this is superior to code together
+
+/**
+ * rubiksCube
+ */
+public class Algorithm {
+    Cube cube;
+
+    public Algorithm(Cube cube) {
+        this.cube = cube;
+    }
+
+    public Cube stepOne(Cube cube) {
+
+        return cube;
+    }
+
+    public Cube stepTwo(Cube cube) {
+        return cube;
+    }
+
+    public static void main(String[] args) {
+    
+    }
+} 
---
+++ Cube.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+/*
+    colors on the faces of the cube will be represented by ints
+    0: green
+    1: white
+    2: yellow
+    3: red
+    4: blue
+    5: orange
+    Consider not just the outer colors, but also the inner colors as well.
+*/
+
+public class Cube {
+    // instance variables for the Rubik's Cube faces and the hash map for the sides' colors
+    private HashMap<String,int[][]> sides;
+    private int[][] face;
+    private int[][] top;
+    private int[][] bottom;
+    private int[][] left;
+    private int[][] right;
+    private int[][] back;
+    
+    public Cube(){
+        this.face = randomize();
+        this.top = randomize();
+        this.bottom = randomize();
+        this.left = randomize();
+        this.right = randomize();
+        this.back = randomize(); 
+        sides.put("face", face);
+        sides.put("top", top);
+        sides.put("bottom", bottom);
+        sides.put("left", left);
+        sides.put("right", right);
+        sides.put("back", back);
+    }
+
+    public Cube(int[][] face, int[][]top, int[][] bottom, int[][]left, int[][] right, int[][] back){
+        this.face = face;
+        this.top = top;
+        this.bottom = bottom;
+        this.left = left;
+        this.right = right;
+        this.back = back; 
+        sides.put("face", face);
+        sides.put("top", top);
+        sides.put("bottom", bottom);
+        sides.put("left", left);
+        sides.put("right", right);
+        sides.put("back", back);
+    }
+
+    private int[][] randomize() {
+        Random rand = new Random();
+        int[][] side = new int[3][3];
+        for (int i = 0; i < side.length; i++) {
+            for(int j = 0; j<side.length; j++){
+                side[i][j] = rand.nextInt(6);
+            }
+        }
+        return side;
+    }
+
+    public int[][] getSide(String sideName){
+       return sides.get(sideName);
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        for (int[][] side : sides.values()) {
+            output += sideToString(side);
+        }
+        return output;
+    }
+
+    private String sideToString(int[][] side) {
+        String output = "";
+        for(int i[] : side) {
+            output += i+"\n";
+        }
+
+        return output;
+    }
+
+
+
+} 
